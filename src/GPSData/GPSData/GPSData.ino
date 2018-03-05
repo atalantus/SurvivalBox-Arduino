d79a84547d58178341463ef8753f04c8c51b826f
atalanthus <jonassebast@yahoo.de>
2018-03-05 17:35:57 +0100

    Added GPS stuff

--- src/GPSData/GPSData/GPSData.ino
+++ src/GPSData/GPSData/GPSData.ino
@@ -1,9 +1,15 @@
+#include <NMEAGPS.h>
+#include <SoftwareSerial.h> 
+
 int gpsInput = 13;
 int gpsOutput = 12;
 int gpsData = 0;
 
+SoftwareSerial serial(gpsInput, gpsOutput);
+
 void setup() {
   Serial.begin(9600);
+  serial.begin(9600);
   // RX beim GPS
   pinMode(gpsOutput, OUTPUT);
   // TX beim GPS
@@ -11,5 +17,28 @@ void setup() {
 }
 
 void loop() {
-  Serial.println(digitalRead(gpsInput));
+  /*
+  while (gps.available(serial)) {
+    fix = gps.read();
+
+    if (fix.valid.status) {
+      Serial.println("Status");
+      Serial.println(fix.valid.status);
+    } else Serial.println("Invalid status");
+    
+    if (fix.valid.time) {
+      Serial.println("Time");
+      Serial.println(fix.valid.time);
+    } else Serial.println("Invalid Time");
+    
+    if (fix.valid.location) {
+      Serial.println("Location");
+      Serial.println(fix.valid.location);
+    } else Serial.println("Invalid location");
+  }*/
+ 
+  if (serial.available())
+    Serial.println(serial.read());
+  else
+    Serial.println("Serial not available!");
 }
